9a2077562f37f35502edc209ee94efebd35fffdb
Volodymyr Kolchin <vkolchin@codifi.me>
2021-12-23 18:13:47 +0200

    list client axios get

--- src/pages/ClientListPage.js
+++ src/pages/ClientListPage.js
@@ -1,56 +1,94 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import {Button} from './../components/Button';
+//import {Button} from './../components/Button';
 
 export const ClientListPage = () => {
     const [client, setClient] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
     const loadMessage = async () => {
-        try {
-            const response = await axios.get('https://json.versant.digital/.netlify/functions/fake-api/message');
-            Object.keys(response.data).forEach((key, index)=> {
-                console.log('key', key);
-                console.log('index', index);
-                client.push({
-                    id: key,
-                    name: `Client ${index+1}`
-                })
-                console.log('client', client);
-                setClient({client});
-
-          })
-        } catch (e) {
-          this.setState({ message: e.message });
-        }
+        const response = await axios.get('https://cleanertrackpro-c446c-default-rtdb.europe-west1.firebasedatabase.app/Test.json');
+        Object.keys(response.data).forEach((key, index)=> {
+            client.push({
+                id: key,
+                name: `Client ${index+1}`
+            })
+            setClient({client});
+            setIsLoaded(true);
+        })
     };
+
+    // Note: an empty array of dependencies [] means that
+    // this useEffect will run once
+    // similar to componentDidMount ()
     useEffect(()=> {
-//        axios.get('https://cleanertrackpro-c446c-default-rtdb.europe-west1.firebasedatabase.app/Test.json')
-//        .then((response)=>{
-//            console.log(response);
-//            const client = [];
-//            Object.keys(response.data).forEach((key, index)=> {
-//                console.log('key', key);
-//                console.log('index', index);
-//                client.push({
-//                    id: key,
-//                    name: `Client ${index+1}`
-//                })
-//                console.log('client', client);
-//                setClient({client});
-//
-//            })
-//        })
-//        .catch(error => {
-//            console.log('error', error);
-//        })
         loadMessage();
     }, [])
 
-    return(
-        <div>
-            <h1>
-                Client List Page
-            </h1>
-
-        </div>
-    )
+    if (!isLoaded) {
+        return <div>Загрузка...</div>;
+    } else {
+        return(
+            <div>
+                <h1>
+                    Client List Page
+                </h1>
+                {client.client.map(item => {
+                return(
+                    <li key={item.id}>
+                        {item.name}
+                    </li>)
+                })}
+            </div>
+        )
+    }
 }
+
+//export const ClientListPage = () => {
+//  const [error, setError] = useState(null);
+//  const [isLoaded, setIsLoaded] = useState(false);
+//  const [data, setData] = useState([]);
+//
+//  // Примечание: пустой массив зависимостей [] означает, что
+//  // этот useEffect будет запущен один раз
+//  // аналогично componentDidMount()
+//  useEffect(() => {
+//    axios.get("https://cleanertrackpro-c446c-default-rtdb.europe-west1.firebasedatabase.app/Test.json")
+//      .then((response)=>{
+//          console.log(response);
+//          const client = [];
+//          Object.keys(response.data).forEach((key, index)=> {
+//              console.log('key', key);
+//              console.log('index', index);
+//              client.push({
+//                  id: key,
+//                  name: `Client ${index+1}`
+//              })
+//              setIsLoaded(true);
+//              setData({client});
+//
+//
+//          })
+//      })
+//
+//  }, [])
+//
+//  if (error) {
+//    return <div>Ошибка:</div>;
+//  } else if (!isLoaded) {
+//    return <div>Загрузка...</div>;
+//  } else {
+//  console.log('data 2 client', data.client)
+//    return (
+//      <div>
+//        {data.client.map(el => {
+//        console.log('ell', el);
+//        return (
+//          <li key={el.id}>
+//            {el.name} {el.price}
+//          </li>
+//          )
+//        })}
+//      </div>
+//    );
+//  }
+//}
