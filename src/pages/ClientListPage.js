b473cb4d319b3761bef3ed38fd42b8c1206a0f23
Volodymyr Kolchin <vkolchin@codifi.me>
2021-12-24 15:03:29 +0200

    I added delete axios

--- src/pages/ClientListPage.js
+++ src/pages/ClientListPage.js
@@ -1,22 +1,37 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import axios from 'axios';
 import {Link} from "react-router-dom";
 
 export const ClientListPage = () => {
-    const [client, setClient] = useState([]);
+    const [data, setData] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
-    const loadMessage = async () => {
-        const response = await axios.get('https://cleanertrackpro-c446c-default-rtdb.europe-west1.firebasedatabase.app/Test.json/');
-        console.log('response. data', response.data);
-        Object.entries(response.data).forEach((key, index)=> {
-            client.push({
-                id: key,
-                name: `Client ${index+1}`
-            })
-            setClient({client});
-            setIsLoaded(true);
-        })
-    };
+//    const loadMessage = async () => {
+//        const response = await axios.get('https://cleanertrackpro-c446c-default-rtdb.europe-west1.firebasedatabase.app/Test.json/');
+//        Object.entries(response.data).forEach((key, index)=> {
+//            client.push({
+//                id: key,
+//                name: `Client ${index+1}`
+//            })
+//            setClient({client});
+//            setIsLoaded(true);
+//        })
+//    };
+    const loadMessage = useCallback(async () => {
+        try {
+            const response = await axios.get('https://cleanertrackpro-c446c-default-rtdb.europe-west1.firebasedatabase.app/Test.json/');
+                let newArr = [];
+                Object.entries(response.data).forEach((key, index)=> {
+                    newArr.push({
+                        id: key,
+                        name: `Client ${index+1}`
+                    })
+                    setIsLoaded(true);
+                })
+                setData(newArr);
+        } catch (e) {
+            console.log(e);
+        }
+    }, []);
 
     // Note: an empty array of dependencies [] means that
     // this useEffect will run once
@@ -25,27 +40,25 @@ export const ClientListPage = () => {
         loadMessage();
     }, [])
 
-    const pressHandler = (event)  => {
-        axios.delete(`https://cleanertrackpro-c446c-default-rtdb.europe-west1.firebasedatabase.app/Test/${event.target.value}.json`)
+    const onRemove = (event)  => {
+        console.log(event);
+        axios.delete(`https://cleanertrackpro-c446c-default-rtdb.europe-west1.firebasedatabase.app/Test/${event}.json`)
         .then((response)=>{
-            console.log(response);
+            loadMessage();
         })
-        .catch(error => {
-            console.log('error', error);
-        })
-        event.preventDefault();
     }
 
     if (!isLoaded) {
         return <div>Загрузка...</div>;
     } else {
+    console.log('data 222', data)
         return(
             <div>
                 <h1>
                     Client List Page
                 </h1>
                 <div className="row">
-                    {client.client.map(item => {
+                    {data.map(item => {
                     return(
                         <div className="col s12 m6" key={item.id[0]}>
                             <div className="card blue-grey darken-1">
@@ -64,7 +77,7 @@ export const ClientListPage = () => {
                                     <Link to={`/client/${item.id[0]}/edit`}>Edit</Link>
                                     <button
                                         className="btn btn-primary"
-                                        onClick={pressHandler}
+                                        onClick={() => onRemove(item.id[0])}
                                         value={item.id[0]}
                                     >
                                         Delete
